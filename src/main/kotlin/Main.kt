1cbd14b503aeb3fff4e18fb8d50941ac0d3bd7b4
Matheus Leite <matheusleite@Matheuss-MacBook-Air.local>
2023-03-09 21:46:42 -0300

    when for and while

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,9 +13,21 @@ fun main() {
     println("Seu saldo atual: $saldo")
 
     when {
-        saldo > 0.0  -> println("conta é positiva")
+        saldo > 0.0 -> println("conta é positiva")
         saldo.equals(0.0) -> println("conta zerada")
-        else         -> println("conta é negativa")
+        else -> println("conta é negativa")
     }
 
+    for (i in 1..3) print(i) //123
+    println()
+    for (i in 3 downTo 1) print(i) //321
+    println()
+    for (i in 3 downTo 1 step 2) print(i) //31
+    println()
+
+    var i = 1
+    while (i <= 5) {
+        println(i)
+        i++
+    }
 }
