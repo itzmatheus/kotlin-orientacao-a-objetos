e82378fdc0d70027da234a580da0e7482c506c8e
Matheus Leite <matheusleite@Matheuss-MacBook-Air.local>
2023-03-11 21:59:36 -0300

    refactor code to use set and get methods

--- src/main/kotlin/Account.kt
+++ src/main/kotlin/Account.kt
@@ -1,22 +1,22 @@
 import java.lang.Exception
 
 data class Account(
-    var owner: Owner? = null,
-    val number: Int? = null,
-    var balance: Float = 0.0f,
+    private var owner: Owner? = null,
+    private val number: Int? = null,
+    private var balance: Float = 0.0f,
 ) {
 
-    fun withdraw(amount: Float) {
-
-        if (!isAmountAvailableToTake(amount)) {
-            throw Exception("DENIED! You just have ${this.balance} of balance.")
+    fun withdraw(value: Float) {
+        if (!isAmountAvailableToTake(value)) {
+            throw Exception("DENIED! You just have ${getBalance()} of balance.")
         }
-
-        this.balance -= amount
+        val amount = getBalance() - value
+        setBalance(amount)
     }
 
-    fun deposit(amount: Float) {
-        this.balance += amount
+    fun deposit(value: Float) {
+        val amount = getBalance() + value
+        setBalance(amount)
     }
 
     fun transfer(accountDestination: Account, amount: Float) {
@@ -26,6 +26,18 @@ data class Account(
 
     private fun isAmountAvailableToTake(
         amountRequired: Float
-    ) = amountRequired <= this.balance
+    ) = amountRequired <= getBalance()
+
+    fun getOwner(): Owner? {
+        return owner
+    }
+
+    private fun setBalance(value: Float) {
+        this.balance = value
+    }
+
+    fun getBalance(): Float {
+        return balance
+    }
 
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,13 +3,13 @@ fun main() {
     val ownerOne = Owner()
     ownerOne.name = "John Doe"
     val accountOne = Account(ownerOne, 1000, 1000.0f)
-    println(accountOne.owner?.name)
+    println(accountOne.getOwner()?.name)
     println(accountOne)
 
     val ownerTwo = Owner()
     ownerTwo.name = "Pathfinder Zemo"
     val accountTwo = Account(ownerTwo, 20000, 1000.0f)
-    println(accountTwo.owner?.name)
+    println(accountTwo.getOwner()?.name)
     println(accountTwo)
 
     println("TAKE 500 for account one")
@@ -18,14 +18,14 @@ fun main() {
     println("DEPOSIT 500 in account two")
     accountTwo.deposit(500f)
 
-    println("accountOne balance ${accountOne.balance}")
-    println("accountTwo balance: ${accountTwo.balance}")
+    println("accountOne balance ${accountOne.getBalance()}")
+    println("accountTwo balance: ${accountTwo.getBalance()}")
 
     println("GIVE 500  FROM ACCOUNT TWO TO ACCOUNT ONE")
     accountTwo.transfer(accountOne, 500f)
 
-    println("accountOne balance ${accountOne.balance}")
-    println("accountTwo balance: ${accountTwo.balance}")
+    println("accountOne balance ${accountOne.getBalance()}")
+    println("accountTwo balance: ${accountTwo.getBalance()}")
 
 
 }
