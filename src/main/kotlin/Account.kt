4f4c8026f4aba85a66600e9f0d173ca2efe7a608
Matheus Leite <matheusleite@Matheuss-MacBook-Air.local>
2023-03-11 21:23:31 -0300

    add methods to transfer, withdraw and deposit

--- src/main/kotlin/Account.kt
+++ src/main/kotlin/Account.kt
@@ -1,5 +1,31 @@
+import java.lang.Exception
+
 data class Account(
     var owner: Owner? = null,
     val number: Int? = null,
     var balance: Float = 0.0f,
-)
+) {
+
+    fun withdraw(amount: Float) {
+
+        if (!isAmountAvailableToTake(amount)) {
+            throw Exception("DENIED! You just have ${this.balance} of balance.")
+        }
+
+        this.balance -= amount
+    }
+
+    fun deposit(amount: Float) {
+        this.balance += amount
+    }
+
+    fun transfer(accountDestination: Account, amount: Float) {
+        withdraw(amount)
+        accountDestination.deposit(amount)
+    }
+
+    private fun isAmountAvailableToTake(
+        amountRequired: Float
+    ) = amountRequired <= this.balance
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,4 +6,26 @@ fun main() {
     println(accountOne.owner?.name)
     println(accountOne)
 
+    val ownerTwo = Owner()
+    ownerTwo.name = "Pathfinder Zemo"
+    val accountTwo = Account(ownerTwo, 20000, 1000.0f)
+    println(accountTwo.owner?.name)
+    println(accountTwo)
+
+    println("TAKE 500 for account one")
+    accountOne.withdraw(500f)
+
+    println("DEPOSIT 500 in account two")
+    accountTwo.deposit(500f)
+
+    println("accountOne balance ${accountOne.balance}")
+    println("accountTwo balance: ${accountTwo.balance}")
+
+    println("GIVE 500  FROM ACCOUNT TWO TO ACCOUNT ONE")
+    accountTwo.transfer(accountOne, 500f)
+
+    println("accountOne balance ${accountOne.balance}")
+    println("accountTwo balance: ${accountTwo.balance}")
+
+
 }
